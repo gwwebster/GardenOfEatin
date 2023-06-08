c4c9357a7c100d28ac56ba9b919c831bbd47bca8
George <gwwebster23@gmail.com>
2023-06-07 19:52:45 -0500

    Added Home component

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Switch, Route } from "react-router-dom";
 import NavBar from './NavBar';
 import About from './About';
 import Menu from "./Menu";
@@ -8,6 +9,7 @@ Component hierarchy
 
 └── App
     ├── NavBar (React Router)
+    ├── Home
     ├── About
     └── Menu
         ├── Dish
@@ -36,7 +38,7 @@ STEPS:
 [ ] Build controlled form
 [ ] Connect components w/ React Router
 [ ] Style
-new titles for each new page
+new titles for each new page - review custom components lesson
 
 */
 
@@ -46,6 +48,7 @@ function App() {
       <title>Garden of Eatin</title>
       <header className="logo">Garden of Eatin</header>
       <NavBar />
+      <Home />
       <About />
       <Menu />
     </div>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,2 +1,11 @@
 import React from 'react';
+import { NavLink } from "react-router-dom";
 
+function NavBar() {
+
+    // return (
+
+    // );
+}
+
+export default NavBar;
