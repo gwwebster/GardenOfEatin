06c21a2c8e5f8d172c2bca037db940226b4c332f
George <gwwebster23@gmail.com>
2023-06-08 19:50:13 -0500

    added basic form structure to NewDish

--- src/components/NewDish.js
+++ src/components/NewDish.js
@@ -1,12 +1,29 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 function NewDish() {
 
     const [formData, setFormData] = useState({});
 
+    function handleSubmit() {
+
+    }
+
     return (
         <div id="form">
-
+            <h3>Add a new dish!</h3>
+            <p>Got an idea for a new dish? We'd love to hear it!
+                Submit your idea for a new dish and see it added to the <Link to="/menu">Menu</Link> page!</p>
+            <form onSubmit={handleSubmit} >
+                <label>Dish Name: <input type="text" placeholder="Dish Name" /></label>
+                <br />
+                <label>Description: <input type="text" placeholder="Description" /></label>
+                <br />
+                <label>Price: <input type="text" placeholder="Price" /></label>                
+                <br />
+                <br />
+                <button>Submit!</button>
+            </form>
         </div>
     );
 }
