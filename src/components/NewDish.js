48d9022c7a35be486f9d40091dc725738f2fe2df
George <gwwebster23@gmail.com>
2023-06-08 19:34:47 -0500

    added basic state to NewDish

--- src/components/NewDish.js
+++ src/components/NewDish.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function NewDish() {
 
+    const [formData, setFormData] = useState({});
+
+    return (
+        <div id="form">
+
+        </div>
+    );
 }
 
 export default NewDish;
