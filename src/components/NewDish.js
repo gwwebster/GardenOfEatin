10b5d82f8e7f9743424fa2ac06fa2bb3231ab15f
George <gwwebster23@gmail.com>
2023-06-08 19:59:34 -0500

    added more form data

--- src/components/NewDish.js
+++ src/components/NewDish.js
@@ -3,9 +3,19 @@ import { Link } from 'react-router-dom';
 
 function NewDish() {
 
-    const [formData, setFormData] = useState({});
+    const [formData, setFormData] = useState({
+        name: "",
+        description: "",
+        price: "",
+    });
 
-    function handleSubmit() {
+    function handleSubmit(e) {
+        e.preventDefault()
+        // POST fetch
+        // callback to set state in Menu
+    }
+
+    function handleChange() {
 
     }
 
@@ -15,11 +25,27 @@ function NewDish() {
             <p>Got an idea for a new dish? We'd love to hear it!
                 Submit your idea for a new dish and see it added to the <Link to="/menu">Menu</Link> page!</p>
             <form onSubmit={handleSubmit} >
-                <label>Dish Name: <input type="text" placeholder="Dish Name" /></label>
+                <label>Dish Name: <input 
+                    type="text" 
+                    placeholder="Dish Name" 
+                    value={formData.name} 
+                    onChange={handleChange} 
+                /></label>
                 <br />
-                <label>Description: <input type="text" placeholder="Description" /></label>
                 <br />
-                <label>Price: <input type="text" placeholder="Price" /></label>                
+                <label>Description: <input 
+                    type="text" 
+                    placeholder="Description" 
+                    value={formData.description} 
+                    onChange={handleChange} /></label>
+                <br />
+                <br />
+                <label>Price: <input 
+                    type="text" 
+                    placeholder="Price" 
+                    value={formData.price} 
+                    onChange={handleChange}
+                /></label>                
                 <br />
                 <br />
                 <button>Submit!</button>
